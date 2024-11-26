e021f807e488879fbb26cc09a32aaf8026f75eec
brijitha <brijithalal36@gmail.com>
2024-11-26 09:15:56 +0530

    employee second  commit

---
+++ connection/DBconnect.py
@@ -0,0 +1,33 @@
+import mysql.connector
+
+class DBConnect:
+    def __init__(self,host="localhost",user="root",password="faith",database="employeemanagement"):
+        # self.connection =None
+        self.host=host
+        self.user=user
+        self.password=password
+        self.database = database
+
+    def connection(self):
+        self.connection=mysql.connector.connect(
+            host =self.host,
+            user=self.user,
+            password = self.password,
+            database = self.database
+        )
+
+        if self.connection.is_connected():
+            print("connected to mysql")
+
+
+    def disconnect(self):
+        if self.connection and self.connection.isConnected():
+            self.connection.close()
+            print("Disconnected")
+
+    # def  create_database(self,database_name):
+
+objConnect = DBConnect(host="localhost",user="root",password="faith",database="employeemanagement")
+objConnect.connection()
+
+
